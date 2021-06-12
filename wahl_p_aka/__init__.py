5f937359aecfbafd8aac4ce1a39bab63b4aaaf59
Leo Maroni <git@em0lar.de>
2021-06-13 00:10:27 +0200

    Dynamic data for start and question site

--- wahl_p_aka/__init__.py
+++ wahl_p_aka/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask
+import flask
 
 app = Flask(__name__)
 
@@ -8,19 +9,40 @@ except FileNotFoundError:
     print("WARNING: No configuration file found.")
     app.config.from_pyfile('application.cfg.example')
 
-from .database import db, Poll
-
-from wahl_p_aka.api import api_blueprint
-
-app.register_blueprint(api_blueprint, url_prefix='/api')
+from .database import db, PollTopic, Poll
 
 
 @app.route("/")
-def hello_world():
-    return render_template("main.html")
+def start_site():
+    polltopics = PollTopic.query.all()
+    return flask.render_template("start.html", topics=polltopics)
 
 
-@app.route("/info")
-def better_world():
-    return render_template("question_site.html")
+@app.route("/question", methods=['post'])
+def question_site():
+    formdata = flask.request.form
+    topic_ids = []
+    for item in formdata.items():
+        if item[1] == "on" and "checkbox" in item[0]:
+            topic_ids.append(item[0].split("_")[1])
 
+    polltopics = []
+    db_polltopics = db.session.query(PollTopic).filter(PollTopic.aw_id.in_(topic_ids))
+    for row in db_polltopics:
+        if len(row.children) != 0:
+            for child in row.children:
+                polltopics.append(child.id)
+        polltopics.append(row.id)
+
+    polls = db.session.query(Poll).filter(
+        Poll.topics.any(PollTopic.id.in_(polltopics))
+    )
+    questions = []
+    for row in polls:
+        questions.append({
+            "id": row.id,
+            "title": row.title,
+            "abstract": row.abstract,
+            "date": row.date
+        })
+    return flask.render_template("question_site.html", questions=questions)
