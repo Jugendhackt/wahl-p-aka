2f1de1917b4f9bd5db5e66c5e3d00196178d9e7d
hflam <henrik.m.flamm@gmail.com>
2021-06-12 17:54:51 +0200

    added the templates and static

--- wahl_p_aka/__init__.py
+++ wahl_p_aka/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from wahl_p_aka.api import api_blueprint
 
@@ -9,4 +9,9 @@ app.register_blueprint(api_blueprint, url_prefix='/api')
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("main.html")
+
+@app.route("/info")
+def better_world():
+    return render_template("question_site.html")
+
--- wsgi.py
+++ wsgi.py
@@ -5,3 +5,4 @@ from wahl_p_aka import app
 if __name__ == "__main__":
     app.run()
 
+
